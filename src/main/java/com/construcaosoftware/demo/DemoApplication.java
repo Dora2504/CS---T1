f2c8e1e0d59ea847c038f721be3796d18f87a53e
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-08-09 22:31:03 -0300

    Creating docker-compose and updateing Dockerfil to build and run two containers

--- src/main/java/com/construcaosoftware/demo/DemoApplication.java
+++ src/main/java/com/construcaosoftware/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.construcaosoftware.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
 public class DemoApplication {
