b928325eeb34ca5e5a10eca6071432208afc2bd2
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-10-04 21:19:50 -0300

    Creating Terraform and AWS connection

---
+++ infra/provider.tf
@@ -0,0 +1,17 @@
+//CONFIGURACAO DO TERRAFORM PARA CONECTAR COM A AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1b"        # Região AWS
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+
