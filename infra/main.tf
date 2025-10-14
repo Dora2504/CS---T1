3d75261729ef8ea64d7ba4defb60a435182b7d70
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-10-14 18:11:00 -0300

    Creating terraform conection

--- infra/main.tf
+++ infra/main.tf
@@ -3,15 +3,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1b"        # Região AWS
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-1"
 }
-
-
