e414b2454f97edf14414a54a4ba778277eafa39a
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-10-23 18:57:04 -0300

    lambda

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,4 @@
-//CONFIGURACAO DO TERRAFORM PARA CONECTAR COM A AWS
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  profile = "academy"
 }
---
+++ src/main/java/com/construcaosoftware/demo/config/StreamLambdaHandler.java
@@ -0,0 +1,31 @@
+package com.construcaosoftware.demo.config;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.construcaosoftware.demo.DemoApplication;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Falha ao inicializar Spring Boot", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
