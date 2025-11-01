1cbc37dfe6dfcd904fbb72316c2c9a915179767d
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-11-01 12:06:59 -0300

    Last update to the Lambda provision

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "app" {
   function_name = "cs_lambda"
-  filename      = "${path.module}/../build/libs/demo-0.0.1-SNAPSHOT-plain.jar"
+  filename      = "./../build/libs/demo-0.0.1-SNAPSHOT-plain.jar"
   handler       = "com.meuapp.StreamLambdaHandler::handleRequest"
   runtime       = "java17"
-  role          =
+  role          = "arn:aws:iam::891377042208:role/LabRole"  # role pré-existente no LabRole (ARN)
   memory_size   = 1024
   timeout       = 30
 }
--- infra/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-caq10151"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
